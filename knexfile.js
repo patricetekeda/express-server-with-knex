4bc0b516f623a771511b1db5a4dfb13404b1fc1e
Patrice Tekeda <patricetekeda@gmail.com>
2023-03-03 10:53:50 -0500

    knex-express

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+const path = require("path");
+
+const { DATABASE_URL } = process.env;
+
+module.exports = {
+  development: {
+    client: "pg",
+    connection: DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+  },
+};
